b0fc2cad60c91de946e5f7a433bc549a7ed80b16
Matthew <matthew@tfaluc.com>
2020-09-05 07:05:13 -0400

    Automatically process a player when they pick up an item. Also prevent false positives from occuring when a player drops an item and then picks it back up.
    Added itemStackOwner to processItem so that if an itemstack is determined a dupe I can more easily implement an alert.

--- src/main/java/com/tfaluc/dupedefender/DupeManager.java
+++ src/main/java/com/tfaluc/dupedefender/DupeManager.java
@@ -1,12 +1,17 @@
 package com.tfaluc.dupedefender;
 
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -14,8 +19,56 @@ import java.util.UUID;
 import static com.tfaluc.dupedefender.DupeDefender.*;
 
 @Mod.EventBusSubscriber(modid = DupeDefender.MODID)
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 public class DupeManager {
     public static final String NBTKey = "dupedefender_uuid";
+
+    /**
+     * When a player joins process their inventory and add watched items to the watchlist.
+     */
+    @SubscribeEvent
+    public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+        processPlayer(event.player);
+    }
+
+    /**
+     * Handles PlayerEvent.ItemPickupEvent
+     * <p>
+     * If the itemstack is still in the player's inventory we process it and then replace it.
+     */
+    @SubscribeEvent
+    public static void onItemPickup(PlayerEvent.ItemPickupEvent event) {
+        InventoryPlayer inven = event.player.inventory;
+        int slot;
+        if ((slot = inven.findSlotMatchingUnusedItem(event.getStack())) != -1) {
+            inven.setInventorySlotContents(slot, processItem(inven.getStackInSlot(slot), event.player));
+        }
+    }
+
+    /**
+     * Handles ItemTossEvent
+     * <p>
+     * When the itemstack is tossed out of the inventory we remove it from the watchList
+     * to prevent a false positive when/if it gets picked back up.
+     * TODO: THIS CAN CAUSE TWO ITEMS TO HAVE DUPLICATE UUIDs VERY RARELY.
+     * TODO: BECAUSE WE ONLY CHECK AGAINST UUIDs IN THE WATCHLIST ON GENERATING A NEW UUID
+     */
+    @SubscribeEvent
+    @SuppressWarnings("ConstantConditions")
+    public static void onItemThrown(ItemTossEvent event) {
+        ItemStack item = event.getEntityItem().getItem();
+        NBTTagCompound tag = item.getTagCompound();
+        if (tag == null) return;
+        try {
+            if (tag.hasUniqueId(NBTKey)) {
+                watchList.getOrDefault(item.getItem().getRegistryName().toString(), new HashSet<>())
+                        .remove(tag.getUniqueId(NBTKey));
+            }
+        } catch (NullPointerException e) {
+            logger.warn("NullPointer exception when trying to process a thrown item, this shouldn't be possible", e);
+        }
+    }
 
     public static void processWorld(World world) {
         for (EntityPlayer player : world.playerEntities) {
@@ -26,7 +79,7 @@ public class DupeManager {
     public static void processPlayer(EntityPlayer player) {
         InventoryPlayer inven = player.inventory;
         for (int slot = 0; slot < inven.getSizeInventory(); slot++) {
-            inven.setInventorySlotContents(slot, processItem(inven.getStackInSlot(slot)));
+            inven.setInventorySlotContents(slot, processItem(inven.getStackInSlot(slot), player));
         }
     }
 
@@ -39,7 +92,7 @@ public class DupeManager {
     }
 
     @SuppressWarnings("ConstantConditions")
-    public static ItemStack processItem(ItemStack itemStack) {
+    public static ItemStack processItem(ItemStack itemStack, EntityPlayer itemStackOwner) {
         if (itemStack == ItemStack.EMPTY) return itemStack;
         String itemName;
         try {
@@ -56,14 +109,13 @@ public class DupeManager {
             NBTTagCompound itemTag = itemStack.getTagCompound();
             if (!itemTag.hasUniqueId(NBTKey)) {
                 logger.debug("  Item did not have UUID assigned, assigned new UUID.");
-                NBTTagCompound dd = new NBTTagCompound();
                 UUID uuid = genUUID(itemName);
                 itemTag.setUniqueId(NBTKey, uuid);
             }
             UUID uuid = itemTag.getUniqueId(NBTKey);
             if (uuidSet.contains(uuid)) {
                 logger.debug("  Item already present on watch list. Uh oh.");
-                // TODO: ADD CODE TO ALERT STAFF
+                // TODO: ALERT STAFF OF THE itemStack AND ALSO itemStackOwner
             } else {
                 uuidSet.add(uuid);
                 watchList.put(itemName, uuidSet);
